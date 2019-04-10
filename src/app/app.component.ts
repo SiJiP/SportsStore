1275036a9670cf80e42fe3128435cc965167edc5
Carter Brown <petruniv.s.i@gmail.com>
2019-04-10 16:24:38 +0300

    new mod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  template: `<div class="bg-success p-a-1 text-xs-center">
+                This is SportsStore
+                </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 }
