7ea549b3293acad55c57f73fa4789eddddd735d2
Carter Brown <petruniv.s.i@gmail.com>
2019-04-11 19:26:43 +0300

    add model

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -5,8 +5,8 @@ import "rxjs/add/observable/from";
 
 @Injectable()
 export class StaticDataSource {
-  private products: Product[]
-new Product(1, "Product 1", "Category 1", "Product 1 (Category 1), 100"),
+  private products: Product[] =[
+  new Product(1, "Product 1", "Category 1", "Product 1 (Category 1), 100"),
   new Product(2, "Product 2", "Category 1", "Product 2 (Category 1), 100"),
   new Product(3, "Product 3", "Category 1", "Product 3 (Category 1), 100"),
   new Product(4, "Product 4", "Category 1", "Product 4 (Category 1), 100"),
@@ -21,8 +21,8 @@ new Product(1, "Product 1", "Category 1", "Product 1 (Category 1), 100"),
   new Product(13, "Product 13", "Category 3", "Product 13 (Category 3), 100"),
   new Product(14, "Product 14", "Category 3", "Product 14 (Category 3), 100"),
   new Product(15, "Product 15", "Category 3", "Product 15 (Category 3), 100"),
-];
-  getproducts(): Observable < Product[] > {
+  ];
+  getProducts(): Observable<Product[]> {
     return Observable.from([this.products]);
   }
 }
